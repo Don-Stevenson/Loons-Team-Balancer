6ff41c020545bb60ca40317c797f59f81237b223
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2026-01-06 16:12:40 -0600

    Add logged in conditional (#315)

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -13,7 +13,7 @@ function AboutPage() {
       <div className="min-h-screen bg-gray-50">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16">
           {/* Header Section */}
-          <Logo />
+          {loggedIn ? null : <Logo />}
 
           {/* Content Section */}
           <div className="max-w-4xl mx-auto">
