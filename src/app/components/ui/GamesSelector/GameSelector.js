5298842941a4d6d76caca7dc1867a2c6dd4ddfeb
Don Stevenson <dstevenson@tribalscale.com>
2026-03-22 05:55:36 -0400

    make players list from heja centered

--- src/app/components/ui/GamesSelector/GameSelector.js
+++ src/app/components/ui/GamesSelector/GameSelector.js
@@ -36,7 +36,7 @@ export default function GameSelector({
         />
         {selectedGameId && (
           <div className="mt-4 items-center justify-center">
-            <h3 className="my-6 text-xl font-bold text-loonsRed">
+            <h3 className="my-6 text-center text-xl font-bold text-loonsRed">
               {queryRsvpsForGame.length} Players RSVP'd for this game on Heja
             </h3>
             {rsvpsLoading ? (
